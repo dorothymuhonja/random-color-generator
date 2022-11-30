a7f19c7e5f8f351d9716a108f866aa1cf9375238
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-11-30 15:28:27 +0300

    random color generator

---
+++ script.js
@@ -0,0 +1,12 @@
+const colorBtn = document.querySelector('.colorBtn')
+const bodyBgc = document.querySelector('body')
+
+const colors = ['magenta', 'lime', 'cyan', '#4b5998']
+
+colorBtn.addEventListener('click', changeColor)
+
+function changeColor() {
+    let random = Math.floor(Math.random()*colors.length)
+    bodyBgc.style.backgroundColor = colors[random]
+}
+
